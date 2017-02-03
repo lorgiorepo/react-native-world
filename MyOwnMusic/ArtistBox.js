b1abca9a46e9cd37a3aed3886c9c07d7fbfce7a3
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2017-02-03 01:07:53 -0300

    Haciendo el componente reutilizable

---
+++ MyOwnMusic/ArtistBox.js
@@ -0,0 +1,83 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default class ArtistBox extends Component {
+  render() {
+    const { image, name, likes, comments } = this.props.artist;
+
+    return (
+        <View style={ styles.artisBox }>
+            <Image style={ styles.image } source={{ uri: image }} />
+            <View style={ styles.info }>
+                <Text style={ styles.name }>{ name } </Text>
+                <View style={ styles.row }>
+                    <View style={ styles.iconContainer }>
+                        <Icon name="ios-heart-outline" size={30} color="gray" />
+                        <Text style={ styles.count }>{likes}</Text>
+                    </View>
+                    <View style={ styles.iconContainer }>
+                        <Icon name="ios-chatboxes-outline" size={30} color="gray" />
+                        <Text style={ styles.count }>{comments}</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  artisBox: {
+      margin: 5,
+      backgroundColor: 'white',
+      flexDirection: 'row',
+      shadowColor: 'black',
+      shadowOpacity: .2,
+      shadowOffset: {
+          height: 1,
+          width: -2,
+      },
+      elevation: 2
+  },
+  image: {
+      width: 150,
+      height: 150,
+  },
+  info: {
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  name: {
+      fontSize: 20,
+      marginTop: 10,
+      color: '#333',
+  },
+  row: {
+      flexDirection: 'row',
+      marginHorizontal: 40,
+      marginTop: 15,
+  },
+  iconContainer: {
+      flex: 1,
+      alignItems: 'center',
+  },
+  count: {
+      color: 'gray',
+  }
+});
--- MyOwnMusic/app.js
+++ MyOwnMusic/app.js
@@ -14,32 +14,24 @@ import {
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
+import ArtistBox from './ArtistBox';
 
 export default class MyOwnMusic extends Component {
   render() {
-    const image = 'https://static.platzi.com/media/files/bowie_a927fdf3-b321-4a5c-99ca-239cc86c57bc.png';
-    const name = 'David Bowie';
-    const likes = 200;
-    const comments = 140;
+    const artist = {
+        image: 'https://static.platzi.com/media/files/bowie_a927fdf3-b321-4a5c-99ca-239cc86c57bc.png',
+        name: 'David Bowie',
+        likes: 200,
+        comments: 140,
+    }
 
     return (
       <View style={ styles.container }>
-        <View style={ styles.artisBox }>
-            <Image style={ styles.image } source={{ uri: image }} />
-            <View style={ styles.info }>
-                <Text style={ styles.name }>{ name } </Text>
-                <View style={ styles.row }>
-                    <View style={ styles.iconContainer }>
-                        <Icon name="ios-heart-outline" size={30} color="gray" />
-                        <Text style={ styles.count }>{likes}</Text>
-                    </View>
-                    <View style={ styles.iconContainer }>
-                        <Icon name="ios-chatboxes-outline" size={30} color="gray" />
-                        <Text style={ styles.count }>{comments}</Text>
-                    </View>
-                </View>
-            </View>
-        </View>
+        <ArtistBox artist={ artist }/>
+        <ArtistBox artist={ artist }/>
+        <ArtistBox artist={ artist }/>
+        <ArtistBox artist={ artist }/>
+        <ArtistBox artist={ artist }/>
       </View>
     );
   }
@@ -50,37 +42,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'lightgray',
     paddingTop: 50,
-  },
-  artisBox: {
-      backgroundColor: 'white',
-      flexDirection: 'row',
-  },
-  image: {
-      width: 150,
-      height: 150,
-  },
-  info: {
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-  },
-  name: {
-      fontSize: 20,
-      marginTop: 10,
-      color: '#333',
-  },
-  row: {
-      flexDirection: 'row',
-      marginHorizontal: 40,
-      marginTop: 15,
-  },
-  iconContainer: {
-      flex: 1,
-      alignItems: 'center',
-  },
-  count: {
-      color: 'gray',
   }
 });
 
